6fcc60152d97eb24bdbfa005ce1c39a2d2ba3780
knotraveller <knotraveller@gmail.com>
2026-05-10 23:01:54 +0800

    5.10

--- dataset/dataset_download.ps1
+++ dataset/dataset_download.ps1
@@ -1,4 +1,4 @@
-# python ./co3d/download_dataset.py --download_folder co3d --single_sequence_subset --n_download_workers 4
+# python ./co3d-main/co3d/download_dataset.py --download_folder co3d --single_sequence_subset --n_download_workers 4
 python download_objaverse.py
 
 ## 特别提醒：如果c盘不够，请先运行 
